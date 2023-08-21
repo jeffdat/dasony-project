cf8cb03c816d86991fe2841a0a746771a6529cf3
developerchoims <developerchoims@gmail.com>
2023-08-21 15:44:19 +0900

    생성

---
+++ dasony/src/main/java/com/ds/dasony/DasonyApplication.java
@@ -0,0 +1,13 @@
+package com.ds.dasony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DasonyApplication.class, args);
+	}
+
+}
---
+++ dasony/src/test/java/com/ds/dasony/DasonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.dasony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
